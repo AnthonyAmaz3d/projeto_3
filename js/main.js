2fa2c5ab96fca45eccf0d7b898e41a799c1d156a
Anthony <anthonymaia00@gmail.com>
2023-07-03 15:39:50 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira um endereço de e-mail válido',
+            mensagem: 'Por favor, insira uma mensagem'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+    })
+})
